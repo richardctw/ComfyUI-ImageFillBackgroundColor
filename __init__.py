4ed835416ba3c7b1aad0e4c4de392ef8176a3607
Richard <dick90012000@users.noreply.github.com>
2026-02-04 14:40:05 +0800

    Fix bug. Add alpha data from input image

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import torch
 import numpy
-from PIL import Image
+from PIL import Image, ImageOps
 
 class ImageFillBackgroundColor:
     @classmethod
@@ -8,6 +8,7 @@ class ImageFillBackgroundColor:
         return {
             "required": {
                 "input_image": ("IMAGE",),
+                "mask": ("MASK",),
                 "color": ("COLOR", {"default": "#FFFFFF"}),
             },
         }
@@ -22,21 +23,33 @@ class ImageFillBackgroundColor:
         hex_color = hex_color.lstrip('#')
         return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
 
-    def process(self, input_image, color):
+    def process(self, input_image, mask, color):
         # Get RGB
         rgb_color = self.hex_to_rgb(color)
         results = []
 
-        for img_tensor in input_image:
-            # Trans input image from Tensor [H, W, C] to Pillow format
-            i = 255. * img_tensor.cpu().numpy()
-            img = Image.fromarray(numpy.clip(i, 0, 255).astype(numpy.uint8)).convert("RGBA")
+        for i in range(len(input_image)):
+            img_tensor = input_image[i]
+            mask_tensor = mask[i] if i < len(mask) else mask[0]
+
+            # Trans input image from Tensor [H, W, C] to Pillow format(RGBA)
+            img_np = (255. * img_tensor.cpu().numpy()).astype(numpy.uint8)
+            img_pil = Image.fromarray(img_np).convert("RGB")
+
+            mask_np = (255. * mask_tensor.cpu().numpy()).astype(numpy.uint8)
+            mask_pil = Image.fromarray(mask_np, mode='L')
+            mask_pil = ImageOps.invert(mask_pil)
+            if mask_pil.size != img_pil.size:
+                mask_pil = mask_pil.resize(img_pil.size, resample=Image.BILINEAR)
+
+            img_rgba = img_pil.copy()
+            img_rgba.putalpha(mask_pil)
 
             # Build a pure color background
-            background = Image.new("RGBA", img.size, rgb_color + (255,))
+            background = Image.new("RGBA", img_rgba.size, rgb_color + (255,))
 
             # Combine to new image
-            combined = Image.alpha_composite(background, img).convert("RGB")
+            combined = Image.alpha_composite(background, img_rgba).convert("RGB")
 
             # Trans back to Tensor [H, W, C] format
             result_tensor = torch.from_numpy(numpy.array(combined).astype(numpy.float32) / 255.0)
